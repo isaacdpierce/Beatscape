ee14a645413bee33c7b3851e749f2c94e52f93d7
Isaac Pierce <contact@isaacdpierce.com>
2019-06-13 17:12:17 -0700

    Redeploy after new build

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
-import App from './App/App';
+import App from 'App/App';
 import * as serviceWorker from './serviceWorker';
 
 import './assets/variables/variables.css';
